f5ba890379823b050390919259c658147115708f
AmshuPatel <amshup@ivoyant.com>
2025-09-04 17:06:11 +0530

    Files name changed

--- src/main/java/com/ivoyant/companydemofordocker/Employee.java
+++ src/main/java/com/ivoyant/companydemofordocker/Employee.java
@@ -1,12 +1,10 @@
 package com.ivoyant.companydemofordocker;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "employee")
 public class Employee {
 
     @Id
